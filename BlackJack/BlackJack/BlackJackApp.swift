38676c023b9c41b362e5af35b2c8da7ca1817b37
Nigel Krajewski <triplemonkeystudio@gmail.com>
2024-03-08 14:00:56 -0500

    initial commit (#1)

---
+++ BlackJack/BlackJack/BlackJackApp.swift
@@ -0,0 +1,32 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Nigel Krajewski on 3/7/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BlackJackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
