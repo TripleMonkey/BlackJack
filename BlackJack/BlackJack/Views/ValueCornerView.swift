8cc3fcd5e9d59402884e5d9dd07752ac1d9108ee
TripleMonkeyStudio <162741925+TripleMonkeyStudio@users.noreply.github.com>
2024-03-17 18:42:07 -0400

    Resolve Conflict in ValueCornerView.swift

--- BlackJack/BlackJack/Views/ValueCornerView.swift
+++ BlackJack/BlackJack/Views/ValueCornerView.swift
@@ -9,36 +9,25 @@ import SwiftUI
 
 struct ValueCornerView: View {
     let value: String
-<<<<<<< HEAD
     let suit: Suit
-=======
-    let suit: String
->>>>>>> 126a808071e182b239484e8cacc7b26c507fe58a
-
     var body: some View {
         VStack {
             Text(value)
-<<<<<<< HEAD
                 .fontWeight(.black)
                 .fontDesign(.rounded)
             Image(systemName: "suit.\(suit).fill")
         }
         .padding()
-=======
                 .font(.largeTitle)
                 .fontWeight(.black)
             .fontDesign(.rounded)
             Image(systemName: suit)
                 .font(.title)
         }
->>>>>>> 126a808071e182b239484e8cacc7b26c507fe58a
     }
 }
 
 #Preview {
-<<<<<<< HEAD
     ValueCornerView(value: "A", suit: .heart)
-=======
     ValueCornerView(value: "A", suit: "heart.fill")
->>>>>>> 126a808071e182b239484e8cacc7b26c507fe58a
 }
